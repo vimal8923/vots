4b54963f395d6001f24ee0c57946ea9abdbc3d22
surajmehra2002 <surimehra8993@gmail.com>
2024-06-21 21:32:44 +0530

    just formating

--- name.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
- int main() 
-{
- printf("hello this is suraj mehra 2002");    
- printf("this is from suraj");
-return 0;
-}
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is main branch and in this branch all sub-branches code are mearged.
