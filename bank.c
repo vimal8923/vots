15694d7f0f58155eee5fca0742c360efa9756b48
surajmehra2002 <surimehra8993@gmail.com>
2024-06-22 07:44:10 +0530

    banking system__completed file

--- bank.c
+++ bank.c
@@ -1,141 +1,177 @@
-#include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-int list();
+#include<stdio.h>
 
-void deposit();
-void withdraw();
-void createaccount();
-void check();
-int totalamount = 500;
-int Amount, withd;
-int totaldeposit = 0, totalwithdraw = 0;
-//long long int number;
-//char Name[50];
 
-void main()
+
+int main ()
 {
-  list();
+    int task, exit=1; 
+    int deposite,withdraw;
 
-    while (1)
-    {
+    char name[20];   
+    long long int account_no=0;
+    int balance;
+
+
+    printf("\n\n***********Project-2     Banking Management System ************\n");
+    printf("\n1- Create Account");
+    printf("\n2- Deposite");
+    printf("\n3- Withdraw");
+    printf("\n4- Check Balance");
+    printf("\n0- Exit\n");
+
     
-    switch (list())
+
+    while (exit!=0)
+    {
+        printf("\nPlease Enter the task number: ");
+        scanf("%d",&task);
+
+        if (task==1)
         {
-       case 1:
-            createaccount();
-            break;
+                if (account_no==0)
+                {
 
-        case 2:
-            deposit();
-            totaldeposit += Amount;
-            break;
-        case 3:
-            withdraw();
-            if (withd <= totalamount)
-                totalamount -= withd;
-            break;
-        case 4:
-            check();
-            break;
-        case 5:
-            exit(0);
-        default:
-        printf("\n invaild choice");
-        }
-        getch();
-    }
-    getch();
-}
-int list()
-{
-    int ch;
-    printf("\n**Banking Management system:");
-    printf("\n1- Create Account:");
-    printf("\n2- Deposit:");
-    printf("\n3- Withdraw:");
-    printf("\n4- Check balance");
-    printf("\n5- Exit:\n");
-    printf("\nPlease enter the task number:");
-    scanf("%d", &ch);
-    return (ch);
-}
-//void create()
-//{
-//    printf("Enter account number 11 digits only : ");
-//    scanf("%lld", &number);
-//}
-void deposit()
-{
-    printf("\nEnter a Amount deposit");
-    scanf("%d", &Amount);
-    totalamount += Amount;
-}
-void withdraw()
-{
-    printf("\nPlease Enter Amount you wish withdraw:");
-    scanf("%d", &withd);
-    if (withd <= totalamount)
-        totalamount -= withd;
-    else
-        printf("\nless Amount withdraw is not possible:");
-}
-void check()
-{
-    printf("\nTotal Amount =%d", totalamount);
-    printf("\nTotal Deposit Amount =%d", totaldeposit);
-    printf("\nTotal Withdraw Amount =%d", totalwithdraw);
-}
-void createaccount()
-{  
-    typedef struct 
-    {
-        long long int accountNumber;
-        char accountHolderName[50];
-    }
+                    create_account:
+                        printf("\nPlease enter 11 digit account number only: ");
+                        scanf("%lld", &account_no);
 
+                    if (account_no > 10000000000 && account_no <100000000000)
+                    {
+                        printf("Please enter your account holder name: ");
+                       scanf("%s", &name);
+                                        
+                                        input_balance:
+                                        printf("Please enter balance (Opening account would be 500 minimum): ");
+                                        scanf("%d", &balance);
 
-    BankAccount;
+                                        if (balance>=500)
+                                        {
+                                            printf("\n\n*****Thank you to join india bank***** \nyour account details:\n");
+                                            printf("Account Number: %lld\n",account_no);
+                                            printf("Account Holder Name: %s\n", name);
+                                            printf("Account Balance: %d\n\n", balance);
 
+                                        }
+                                        else{
+                                            goto input_balance;
+                                        }
+                                        
+                    }    
+                                
+                    else
+                        {
+                            printf("Please enter valid account no");
+                            goto create_account;
+                        }   
+                }          
+                else
+                {
+                    int press;
+                    printf("You have already account\n");
+                    printf("press--1 for new account \npress--2 for account information \npress--0 for exit\n");
+                    printf("press--");
+                    scanf("%d",&press);
+                    if (press==1)
+                    {
+                         goto create_account;
+                    }
 
-    int isValidAccountNumber(long long int accountNumber)
-    {
-        return accountNumber >= 10000000000LL && accountNumber <= 99999999999LL;
-    }
+                    else if (press==2)
+                    {
+                        printf("Account Number: %lld\n",account_no);
+                        printf("Account Holder Name: %s\n", name);
+                        printf("Account Balance: %d\n\n", balance);
+                    }
+                    else if (press==0)
+                    {
+                        printf("Your access is previous account\n");
+                    }
+                    
+                    
+                    
 
-
-    BankAccount createAccount()
-    {
-        BankAccount account;
-
-        printf("Enter account number 11 digits only : ");
-        scanf("%lld", &account.accountNumber);
-        while (!isValidAccountNumber(account.accountNumber))
-        {
-            printf("Invalid account number. Please enter an 11-digit account number only: ");
-            scanf("%lld", &account.accountNumber);
+                }                   
         }
 
-        printf("Enter account holder name: ");
-        scanf(" %s", account.accountHolderName); 
+        else if (task==2)
+        {
+                
+            if (account_no > 10000000000 && account_no <100000000000)
+            {
+                depositemoney:
+                    printf("\nPlease enter deposite balance: ");
+                    scanf("%d", &deposite);
 
-        return account;
+                if (deposite>=100)
+                {
+                    balance+=deposite;
+                    printf("\n***Thanks for using us***\n%d Rupeees has credited in Your account.\n", deposite);
+                }
+                else if(deposite<100 && deposite!=0)
+                {
+                    printf("Please deposite balance more than 100 (press--0 for exit): ");
+                    goto depositemoney;
+                }
+                else
+                {
+                    printf("You left");
+                }
+            }
+            else
+            {
+                printf("Sorry! Please create account first....\n");
+            }
+        
+                
+        }
+
+        else if (task==3)
+        {
+            if (account_no > 10000000000 && account_no <100000000000)
+            { 
+                printf("Enter withdraw ammount: ");
+                scanf("%d", &withdraw);
+                if (balance>withdraw)
+                {
+                    balance-=withdraw;
+                    printf("%d rupees debited in your bank account\n",withdraw);
+                }
+                else
+                {
+                    printf("Insufficient Balance in your account\n");
+                }
+
+            }
+            else
+            {
+                printf("Sorry! Please create account first....\n");
+            }
+        }
+
+        else if (task==4)
+        {    if (account_no > 10000000000 && account_no <100000000000)
+            {
+                printf("Your account Balance is: %d\n", balance);
+            }
+            else
+            {
+                printf("Sorry! Please create account first....\n");
+            }
+
+        }
+        else if (task==0)
+        {
+            exit = task;
+        }
+
+        else
+        {
+            printf("\nOhh! Please Enter valid task no\n");
+            
+        }
     }
 
+    printf("You have left");
 
-    void displayAccount(BankAccount account) 
-    {
-        printf("\nAccount Number: %d\n", account.accountNumber);
-        printf("Account Holder Name: %s\n", account.accountHolderName);
-    }
-
-
-    BankAccount account;    
-
-    account = createAccount();
-
-    printf("\n***** Welcome, %s. Your account is created *****\n\n", account.accountHolderName);
-
-    displayAccount(account);
-
+    return 0;
 }
